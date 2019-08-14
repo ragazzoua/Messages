6ce936632b54d41b68776c14961e1897e1e402c4
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-08-14 15:17:43 +0300

    Writer class implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -25,5 +27,35 @@ class Message {
         }
         empty = false;
         this.message = message;
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        String messages[] = {
+                "Humpty Dumpty sat on a wall",
+                "Humpty Dumpty had a great wall",
+                "All the king's horses and all the king's men",
+                "Couldn't put Humpty together again"
+        };
+
+        Random random = new Random();
+
+        for (int i = 0; i < messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        message.write("Finished");
     }
 }
