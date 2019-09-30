e324657ae8c870f74e75b6d5ebf3479bff829889
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-09-30 11:04:56 +0300

    Reader class implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -59,3 +59,25 @@ class Writer implements Runnable {
         message.write("Finished");
     }
 }
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Fineshed");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
